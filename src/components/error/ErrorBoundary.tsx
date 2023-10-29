fec3f738a42d3c438d680622d9e16b8241e69fbb
Home <picccc89@gmail.com>
2023-10-29 11:47:08 +0300

    fix: a few bags

--- src/components/error/ErrorBoundary.tsx
+++ src/components/error/ErrorBoundary.tsx
@@ -14,17 +14,11 @@ export default class ErrorBoundary extends Component<Props, State> {
   };
 
   static getDerivedStateFromError(error: Error) {
-    // Update state so the next render will show the fallback UI.
     console.log(error);
     return { hasError: true };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    // Example "componentStack":
-    //   in ComponentThatThrows (created by App)
-    //   in ErrorBoundary (created by App)
-    //   in div (created by App)
-    //   in App
     console.error('Uncaught error:', error, errorInfo);
   }
 
